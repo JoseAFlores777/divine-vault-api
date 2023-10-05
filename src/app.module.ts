14afa2e0d31d45fcb4c852887e24d33d7eaf3e9d
José Izaguirre <joseadolfoizaguirreflores@gmail.com>
2023-10-05 16:53:22 -0600

    add dockerization and cloud aws config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CatsModule } from './cats/cats.module';
+import { EnvConfiguration } from './config/env.config';
+
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    CatsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
