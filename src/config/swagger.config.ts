d4d6b3355f46d8c655fa0c105d05ff018b089e29
José Izaguirre <joseadolfoizaguirreflores@gmail.com>
2023-10-06 02:18:07 -0600

    add swagger documentation

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,15 @@
+import { INestApplication } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+export default function setupSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('My REST API documentation')
+    .setVersion('1.0.0')
+    .addTag('Api Tag')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
+}
