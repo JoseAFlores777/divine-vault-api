3c18a8fb196fcc20034a66705a7d6b7b0e521bec
José Izaguirre <joseadolfoizaguirreflores@gmail.com>
2023-10-05 18:43:58 -0600

    add codestatus

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return {
+      statusCode: 200,
+      body: 'Hello from lambda', // <-- here
+    };
   }
 }
